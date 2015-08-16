dfd9b7be1361b67e856be14be0ac97e3de74b51d
danielorozco87 <danielorozco87@gmail.com>
2015-08-16 19:19:05 -0300

    Logica de creacion de recomendados

--- src/main/webapp/WEB-INF/views/user/register.jsp
+++ src/main/webapp/WEB-INF/views/user/register.jsp
@@ -131,8 +131,6 @@
 		  });
 	  };
 	  
-	  //
-	  
 	(function(d, s, id) {
 	    var js, fjs = d.getElementsByTagName(s)[0];
 	    if (d.getElementById(id)) return;
--- src/main/webapp/static/scripts/headerController.js
+++ src/main/webapp/static/scripts/headerController.js
@@ -3,5 +3,15 @@ myRecommendedApp.controller('headerController', function ($scope, $http) {
 		if($scope.recommendedKey) {
 			redirect("recomendados/" + $scope.recommendedKey);
 		}
+		
+		
+		//creacion de nuevo recomendado
+		/*var newRecommended = {categoryIds : [1,2,3], name: "nuevo recomendado", description: "descripcion del nuevo recomendado"};
+		$http.post(getCompletePath("recommended/createRecommended"), JSON.stringify(newRecommended))
+		.success(function (user) {
+	    }).error(function (data, status, headers, config) {
+	    	alert("Ha ocurrido un problema. Por favor intente nuevamente");
+	    });*/
+		
 	};
 });
